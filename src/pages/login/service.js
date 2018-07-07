82ad0b8291db0a259f2ccbb184abecf0d1568ba5
Goran Mekić <meka@tilda.center>
2018-07-07 03:50:32 +0200

    Cleanup

--- src/pages/login/service.js
+++ src/pages/login/service.js
@@ -5,19 +5,14 @@ const API_ROOT = ' http://backend.thinker.my.domain:5000/api/v0'
 
 
 const login = async function({ email, password }) {
-  try {
-    const response = await axios.post(`${API_ROOT}/auth/tokens`, {
+  const response = await axios.post(
+    `${API_ROOT}/auth/tokens`,
+    {
       email,
       password,
-    }, {
-      headers: {
-        Accept: 'application/json',
-      },
-    })
-    return response.data
-  } catch (error) {
-    throw error
-  }
+    }
+  )
+  return response.data
 }
 
 
