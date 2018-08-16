f5f552d559c837d3c13c3ba4189faeed0bb6f56e
Goran Mekić <meka@tilda.center>
2018-08-16 21:43:14 +0200

    Support npm and yarn

--- bin/devel.sh
+++ bin/devel.sh
@@ -2,19 +2,28 @@
 
 BIN_DIR=`dirname $0`
 PROJECT_ROOT=`readlink -f "${BIN_DIR}/.."`
+NPM=`which npm`
+YARN=`which yarn`
+
+if [ ! -z "${NPM}" ]; then
+  PACKAGE_MANAGER="${NPM}"
+else
+  PACKAGE_MANAGER="${YARN}"
+fi
+
 if [ -e "${PROJECT_ROOT}/project.conf" ]; then
 	. "${PROJECT_ROOT}/project.conf"
 else
 	echo -n "Please create ${PROJECT_ROOT}/project.conf" >&2
 	echo " containing API_ROOT" >&2
 	echo "Example: API_ROOT='http://localhost:5000/api/v0'" >&2
-	sleep 15
+	read novar || sleep 15
 	exit 1
 fi
 
 cd ${PROJECT_ROOT}
 echo "export const API_ROOT='${API_ROOT}'" >src/local-conf.js
-npm install
+${PACKAGE_MANAGER} install
 echo "Frontend"
 echo "========"
-npm start
+${PACKAGE_MANAGER} start
