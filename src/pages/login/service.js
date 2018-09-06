beb1f12be2e141fbbedb6a85a5277249a02f760e
Goran Mekić <meka@tilda.center>
2018-09-06 12:45:32 +0200

    Change login URL

--- src/pages/login/service.js
+++ src/pages/login/service.js
@@ -4,7 +4,7 @@ import { API_ROOT } from 'local-conf'
 
 const login = async function({ email, password }) {
   const response = await axios.post(
-    `${API_ROOT}/auth/tokens`,
+    `${API_ROOT}/auth/login`,
     {
       email,
       password,
