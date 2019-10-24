a99d08972d4553bf729cba91fb31632126b2de8c
Goran Mekić <meka@tilda.center>
2019-10-24 11:22:03 +0200

    Set default target

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,15 +1,12 @@
 const proxy = require('http-proxy-middleware')
 module.exports = function(app) {
+  const target = process.env.BACKEND_URL || 'http://localhost:5000'
   app.use(proxy(
     '/api',
-    {
-      target: process.env.BACKEND_URL,
-    }
+    { target },
   ))
   app.use(proxy(
     '/media',
-    {
-      target: process.env.BACKEND_URL,
-    }
+    { target },
   ))
 }
