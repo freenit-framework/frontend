d6e35ce0f2630295580a38a29b29d68237fed76a
Goran Mekić <meka@tilda.center>
2020-02-29 01:40:34 +0100

    Cleanup

--- src/lib/pages/profile/service.js
+++ src/lib/pages/profile/service.js
@@ -1,13 +1,11 @@
 export default {
   edit: async data => {
     const response = await window.rest.patch('/profile', data)
-
     return response.data
   },
 
   fetch: async () => {
     const response = await window.rest.get('/profile')
-
     return response.data
   },
 }
